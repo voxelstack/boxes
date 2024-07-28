44ad9d5ddb9bbd164cec257a46b24adc93409b7d
voxelstack <voxelstack@gmail.com>
2024-07-28 01:16:46 -0300

    Reformat css-to-ts.js.

--- scripts/css-to-ts.js
+++ scripts/css-to-ts.js
@@ -1,11 +1,11 @@
+import { promises as fs } from "fs";
 import watch from "node-watch";
 import { resolve } from "path";
-import { promises as fs } from "fs";
 import { exit } from "process";
 
 async function cssToTs(src) {
-    const dst = src.replace('.lit.css', '.styles.ts');
-    const css = await fs.readFile(src, { encoding: 'utf8' });
+    const dst = src.replace(".lit.css", ".styles.ts");
+    const css = await fs.readFile(src, { encoding: "utf8" });
     const embed = css.replace(/\n\n\/\*#\ sourceMappingURL=[^\*]+ \*\//, "");
 
     console.log(`${src}->${dst}`);
@@ -17,14 +17,16 @@ async function cssToTs(src) {
 
 async function preprocessAll(path) {
     const dir = await fs.readdir(path, { withFileTypes: true });
-    await Promise.all(dir.map((ent) => {
-        const src = resolve(path, ent.name);
-        if (ent.isDirectory()) {
-            return preprocessAll(src);
-        } else if (/\.lit\.css/.test(src)) {
-            return cssToTs(src);
-        }
-    }));
+    await Promise.all(
+        dir.map((ent) => {
+            const src = resolve(path, ent.name);
+            if (ent.isDirectory()) {
+                return preprocessAll(src);
+            } else if (/\.lit\.css/.test(src)) {
+                return cssToTs(src);
+            }
+        }),
+    );
 }
 
 await preprocessAll(process.argv[2]);
@@ -33,16 +35,12 @@ if (process.argv[3] !== "--watch") {
     exit(0);
 }
 
-console.log("Watching for changes on .lit.css files...")
-watch(
-    "./",
-    { filter: /\.lit\.css/, recursive: true },
-    async (evt, src) => {
-        switch (evt) {
-            case "update": {
-                await cssToTs(src);
-                break;
-            };
+console.log("Watching for changes on .lit.css files...");
+watch("./", { filter: /\.lit\.css/, recursive: true }, async (evt, src) => {
+    switch (evt) {
+        case "update": {
+            await cssToTs(src);
+            break;
         }
     }
-);
+});
